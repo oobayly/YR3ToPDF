c0fcbf6cc5006f9c2fe936333eaf2ee90bf17697
oobayly <oobayly@yahoo.com>
2016-12-05 09:50:14 +0000

    Initial commit

---
+++ YR3ToPDF/My Project/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("YR3ToPDF")> 
+<Assembly: AssemblyDescription("Simple tool for converting YR3 S08 files to PDF")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("YR3ToPDF")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a6945621-8a19-40ce-a9a6-ef105bb28011")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.*")> 
